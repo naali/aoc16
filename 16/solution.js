773e82cd000f091fe03dc9527e2284f6d7225919
Antti Svenn <antti.svenn@gmail.com>
2016-12-29 00:59:38 +0200

    Day 16 done

--- 16/solution.js
+++ 16/solution.js
@@ -1,17 +1,63 @@
 function solver(input) {
 	return { 
-		task1: solve1(input),
-		task2: solve2(input)
+		task1: solve(input, 272),
+		task2: solve(input, 35651584)
 	}
 }
 
-function solve1(input) {
-	var arr = input.split("\n");
-	return "foo";
+function dragonize(d) {
+    var a = d;
+    var b = '';
+    
+    for (var i=d.length - 1; i>=0; --i) {
+        b += d.charAt(i) == '0' ? '1' : '0';
+    }
+    
+    return a + '0' + b;
 }
 
-function solve2(input) {
+function checksum(str) {
+    var cs = str;
+
+    do {
+        var tmpcs = '';
+        
+        for (var i=0; i<cs.length/2; i++) {
+            var cp = '' + cs.charAt(i * 2) + cs.charAt(i * 2 + 1);
+            switch (cp) {
+                case '00':
+                case '11':
+                    tmpcs += '1';
+                    break;
+                case '01':
+                case '10':
+                    tmpcs += '0';
+                    break;
+                default:
+                    // wtf
+                    debugger;
+            }
+        }
+        
+        cs = tmpcs;
+        
+    } while (cs.length % 2 != 1)
+
+    return cs;
+}
+
+function solve(input, disksize) {
 	var arr = input.split("\n");
-	return "bar";
+	var dragon = arr[0];
+	
+	do {
+	    dragon = dragonize(dragon);
+	} while (dragon.length <= disksize);
+	
+	dragon = dragon.substring(0, disksize);
+	
+	var cs = checksum(dragon);
+	
+	return cs;
 }
 
