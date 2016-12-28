44ca6b1110d35d2488fc46538c63e54f14735628
Antti Svenn <antti.svenn@gmail.com>
2016-12-29 01:28:37 +0200

    Day 18 done

--- 18/solution.js
+++ 18/solution.js
@@ -1,17 +1,73 @@
 function solver(input) {
 	return { 
-		task1: solve1(input),
-		task2: solve2(input)
+		task1: solve(input, 40),
+		task2: solve(input, 400000)
 	}
 }
 
-function solve1(input) {
-	var arr = input.split("\n");
-	return "foo";
+function printRogue(r, w, h) {
+    var prnt = '';
+    for (var i=0; i<h; i++) {
+        var str = '';
+        
+        for (var j=0; j<w; j++) {
+            str += r[i * w + j];
+        }
+        
+        prnt += str + "\n";
+    }
+    
+    l(prnt);
 }
 
-function solve2(input) {
+function solve(input, rows) {
 	var arr = input.split("\n");
-	return "bar";
+	var str = arr[0];
+	
+	var rogue = [];
+	
+	for (var i=0; i<str.length; i++) {
+	    rogue.push(str.charAt(i));
+	}
+	
+	for (var i=0; i<(rows-1); i++) {
+	    for (var j=0; j<str.length; j++) {
+	        var left = j==0 ? '.' : rogue[str.length * i + (j - 1)];
+	        var center = rogue[str.length * i + j];
+	        var right = j==str.length - 1? '.' : rogue[str.length * i + (j + 1)];
+
+            var d = '' + left + center + right;
+            var tile = '';
+            
+            switch (d) {
+                case '^^.':
+                case '.^^':
+                case '^..':
+                case '..^':
+                    tile = '^';
+                    break;
+                default:
+                    tile = '.';
+                    break;
+            }
+            
+            rogue.push(tile);
+
+	    }
+	    
+	    if (i % 10000 == 1) {
+	        l(i);
+	    }
+	}
+	
+    var safe = 0;
+    
+    for (var i=0; i<rogue.length; i++) {
+        if (rogue[i] == '.') {
+            safe++;
+        }
+    }
+	
+	return safe;
 }
 
