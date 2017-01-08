21f99e263034f53064c9bfeb4817ecd4c3293d2d
Antti Svenn <antti.svenn@gmail.com>
2017-01-08 16:16:19 +0200

    day 22 part 1

--- 22/solution.js
+++ 22/solution.js
@@ -7,7 +7,46 @@ function solver(input) {
 
 function solve1(input) {
 	var arr = input.split("\n");
-	return "foo";
+	var re = /\/dev\/grid\/node\-x(\d+)\-y(\d+)\s+(\d+).\s+(\d+).\s+(\d+).\s+(\d+)./;
+	
+	var nodes = [];
+	
+	for (var i=0; i<arr.length; i++) {
+	    var m = arr[i].match(re);
+	    
+	    if (m) {
+	        var node = {
+                x: parseInt(m[1]),
+	            y: parseInt(m[2]),
+	            size: parseInt(m[3]),
+	            used: parseInt(m[4]),
+	            avail: parseInt(m[5]),
+	            usepct: parseInt(m[6])
+	        };
+	        
+	        nodes.push(node);
+	    }
+	}
+	
+	var pairs = [];
+	
+	for (var i=0; i<nodes.length; i++) {
+	    for (var j=0; j<nodes.length; j++) {
+            var a = nodes[i];
+            var b = nodes[j];
+            
+            if ((a.x != b.x || a.y != b.y) && a.used != 0 && a.used <= b.avail) {
+                var pair = {
+                    a: a,
+                    b: b
+                }
+                
+                pairs.push(pair);
+            }
+	    }
+	}
+	
+	return pairs.length;
 }
 
 function solve2(input) {
